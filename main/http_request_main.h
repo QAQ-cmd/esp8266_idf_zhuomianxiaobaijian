f72af3d87d718f4c1bb2a8111edde0122e357415
yangx <1046436450@qq.com>
2024-09-18 21:12:30 +0800

    YX: add使用http get方法获取高德地图API的天气

---
+++ main/http_request_main.h
@@ -0,0 +1,4 @@
+#ifndef HTTP_MAIN_H
+#define HTTP_MAIN_H
+void http_main();
+#endif // HTTP_MAIN_H
--- main/main.c
+++ main/main.c
@@ -3,17 +3,26 @@
 #include "freertos/task.h"
 
 #include "smartconfig_main.h"
+#include "http_request_main.h"
 
 
 void app_main(void)
 {
+    // 打印IDF版本
+    printf("SDK version:%s\n", esp_get_idf_version());
+    // Hello world!
     int i = 0;
     while (i < 1) {
         printf("[%d] Hello world!\n", i);
         i++;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+
     ESP_ERROR_CHECK(nvs_flash_init());
+    // 开启WiFi智能配网线程
     initialise_wifi();
+    // 连接高德API
+    http_main();
+
     printf("Hello Yangx  !\n");
 }
